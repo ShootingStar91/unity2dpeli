6cd22b88c88631a66aae454cf94e2bab439e34ca
Arttu Kangas <ttu91@hotmail.com>
2022-04-06 15:31:07 +0300

    lisätty takedamage & die animaatiot, healthscript ja attack funktio

--- Assets/Scripts/FighterScript.cs
+++ Assets/Scripts/FighterScript.cs
@@ -1,36 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class FighterScript : MonoBehaviour
 {
-
+ 
     public Animator Animator;
-
+ 
     public bool BlockCheck = false;
     public int Chooser;
-
+ 
     public float Cooldown = 0.25f;
     private float CooldownTimer;
-
+ 
     private bool Attacking = false;
     private bool Hit = false;
-
+ 
     public Transform PunchCheck;
     public Transform KickCheck;
-
+ 
     public float Range = 1.75f;
-
+ 
     public LayerMask EnemyLayer;
     public float PunchDamage = 2f;
     public float KickDamage = 3f;
-
+ 
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+ 
     // Update is called once per frame
     void Update()
     {
@@ -41,13 +41,7 @@ public class FighterScript : MonoBehaviour
             if (Input.GetButtonDown("Fire2")) {
                 Kick();
             }
-            if (Attacking) {
-                if (CooldownTimer > 0) {
-                    CooldownTimer -= Time.deltaTime;
-                } else {
-                    Attacking = false;
-                }
-            }
+ 
             if (Input.GetButtonDown("Fire3")) {
                 Block();
             }
@@ -55,8 +49,15 @@ public class FighterScript : MonoBehaviour
                 BlockEnd();
             }
         }
+                    if (Attacking) {
+                if (CooldownTimer > 0) {
+                    CooldownTimer -= Time.deltaTime;
+                } else {
+                    Attacking = false;
+                }
+            }
     }
-
+ 
     void Punch() {
         Chooser = Random.Range(0, 2);
         if (Chooser == 0) {
@@ -64,8 +65,10 @@ public class FighterScript : MonoBehaviour
         } else {
             Animator.SetTrigger("Punch2");
         }
+        Attack(PunchCheck, PunchDamage);
+ 
     }
-
+ 
     void Block() {
         Animator.SetTrigger("Block");
         Animator.SetBool("Blocking", true);
@@ -76,7 +79,27 @@ public class FighterScript : MonoBehaviour
         Animator.SetBool("Blocking", false);
         BlockCheck = false;
     }
-
+ 
+    //Making the enemy take damage and updating it to everyone
+    void Attack(Transform Check, float Damage){
+        Collider2D[] EnemyHit = Physics2D.OverlapCircleAll(Check.position, Range, EnemyLayer);
+        
+        if(EnemyHit != null){
+            foreach(Collider2D Enemy in EnemyHit){
+                if(Hit == false){
+                    if(Enemy.gameObject != this.gameObject){
+                        // Enemy.GetComponent<HealthScript>().TakeDamage(Damage);
+                        Debug.Log("Osuma");
+                        Hit = true;
+                    }
+                }
+            }
+            Hit = false;
+        }
+        Attacking = true;
+        CooldownTimer = Cooldown;
+    }
+ 
     void Kick() {
         Chooser = Random.Range(0, 2);
         if (Chooser == 0) {
@@ -84,8 +107,10 @@ public class FighterScript : MonoBehaviour
         } else {
             Animator.SetTrigger("Kick2");
         }
+        Attack(KickCheck, KickDamage);
+ 
     }
-
+ 
     void OnDrawGizmosSelected() {
         if (PunchCheck == null) {
             return;
@@ -93,5 +118,5 @@ public class FighterScript : MonoBehaviour
         Gizmos.DrawWireSphere(PunchCheck.position, Range);
         Gizmos.DrawWireSphere(KickCheck.position, Range);
     }
-
+ 
 }
